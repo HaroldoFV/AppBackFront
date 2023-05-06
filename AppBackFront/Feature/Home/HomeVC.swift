8792f40122bf71650eb1e6d7b29ab772aa18d67a
Haroldo <haroldovinente@gmail.com>
2023-05-06 19:17:14 -0300

    configurando constraints para o HomeScreen

--- AppBackFront/Feature/Home/HomeVC.swift
+++ AppBackFront/Feature/Home/HomeVC.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 class HomeVC: UIViewController {
+    var screen: HomeScreen?
+
+    override func loadView() {
+        screen = HomeScreen()
+        view = screen
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
--- AppBackFront/Feature/Home/Screen/HomeScreen.swift
+++ AppBackFront/Feature/Home/Screen/HomeScreen.swift
@@ -32,7 +32,7 @@ class HomeScreen: UIView {
     }()
 
     lazy var collectionView: UICollectionView = {
-        let collectionView = UICollectionView()
+        let collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewLayout())
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         collectionView.showsHorizontalScrollIndicator = false
         collectionView.backgroundColor = UIColor(red: 26/255, green: 26/255, blue: 1/255, alpha: 1.0)
@@ -56,6 +56,7 @@ class HomeScreen: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         addViews()
+        configConstraints()
     }
 
     func addViews() {
@@ -69,5 +70,35 @@ class HomeScreen: UIView {
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    func configConstraints() {
+        NSLayoutConstraint.activate([
+            viewBackGround.topAnchor.constraint(equalTo: topAnchor),
+            viewBackGround.leadingAnchor.constraint(equalTo: leadingAnchor),
+            viewBackGround.trailingAnchor.constraint(equalTo: trailingAnchor),
+            viewBackGround.heightAnchor.constraint(equalToConstant: 220),
+
+            logoImageView.topAnchor.constraint(equalTo: viewBackGround.topAnchor, constant: 45),
+            logoImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24),
+            logoImageView.heightAnchor.constraint(equalToConstant: 40),
+            logoImageView.widthAnchor.constraint(equalToConstant: 40),
+
+            searchBar.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 10),
+            searchBar.leadingAnchor.constraint(equalTo: logoImageView.leadingAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -24),
+            searchBar.heightAnchor.constraint(equalToConstant: 46),
+
+            collectionView.topAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 15),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: viewBackGround.bottomAnchor),
+
+            tableView.topAnchor.constraint(equalTo: viewBackGround.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+
+        ])
     }
 }
