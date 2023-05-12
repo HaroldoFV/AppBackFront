ee8463aad89a56252290469ec4ee9f40a2d59141
Haroldo <haroldovinente@gmail.com>
2023-05-12 17:18:16 -0300

    criação da tela de detalhe

---
+++ AppBackFront/Feature/NftDetail/ViewModel/NftDetailViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  NftDetailViewModel.swift
+//  AppBackFront
+//
+//  Created by Haroldo Vinente on 11/05/23.
+//
+
+import UIKit
+
+class NftDetailViewModel: NSObject {
+    let nft: Nft
+
+    init(nft: Nft) {
+        self.nft = nft
+    }
+}
